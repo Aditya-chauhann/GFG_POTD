daf9556d63a86ba0b891899e273428c982232296
Aditya-chauhann <aaditya143chauhan@gmail.com>
2024-02-25 15:44:18 +0530

    POTD

---
+++ 14-feb-2024.cpp
@@ -0,0 +1,25 @@
+
+//{ Driver Code Starts.
+
+int main(){
+	int tc;
+    cin >> tc;
+    while(tc--){
+        int V, E;
+        cin >> V >> E;
+        vector<int>adj[V];
+        for(int i = 0; i < E; i++){
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        vector<vector<int>>ans = obj.criticalConnections(V, adj);
+        
+        for(int i=0;i<ans.size();i++)
+            cout<<ans[i][0]<<" "<<ans[i][1]<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
