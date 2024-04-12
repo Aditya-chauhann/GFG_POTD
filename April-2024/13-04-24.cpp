00843810a928d9f26ff537886968427ea14c3abe
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2024-04-13 03:46:51 +0530

    GFG_POTD

---
+++ April-2024/13-04-24.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        long long ans=0;
+        for(int i= 0 ; i<32; i++)
+        {
+            int bit= (x>>i)&1;
+            if(bit)
+            {
+                ans= ans | (1ll<<(31-i));
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
