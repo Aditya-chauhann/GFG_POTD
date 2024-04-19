0fdeb204922a3c1bedfde9679c5ac25422824d46
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2024-04-20 01:53:34 +0530

    GFG_POTD

---
+++ April-2024/20-04-24.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        set<int> s;
+        for(int i=0;i<n;i++){
+            s.insert(arr1[i]);
+        }
+        for(int i=0;i<m;i++){
+            s.insert(arr2[i]);
+        }
+        vector<int> ans;
+        for(auto it:s){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
