986e02dacaddb51e2a2bb2b4a6f42bd11622cbd8
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2024-05-25 21:58:09 +0530

    GFG-POTD

---
+++ May-2024/25-05-24.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long max_Books(int arr[], int n, int k) {
+        int l = 0, r = 0;
+        long long maxi = 0;
+        long long sum = 0;
+        while(r < n){
+            if(arr[r] > k){
+                r++;
+                l = r;
+                sum = 0;
+            }
+            else{
+                sum += arr[r];
+                r++;
+                maxi = max(maxi ,sum);
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
