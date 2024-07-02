d2a161bd379233d5d22f4613d36890649827a08f
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2024-07-02 17:23:01 +0530

    GFG_POTD

---
+++ July-2024/02-07-24.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// Code by Shubham Joshi
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    string data;
+    Node *next;
+
+    Node(string x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *newNode(string str) {
+    Node *new_node = new Node(str);
+    new_node->next = NULL;
+    return new_node;
+}
+
+void print(Node *root) {
+    Node *temp = root;
+
+    while (temp != NULL) {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+struct Node
+{
+    string data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    bool compute(Node* head) 
+    {
+        string s = "";
+        Node *temp = head;
+        while(temp)
+        {
+            s += temp->data;
+            temp = temp->next;
+        }
+        int len = s.size();
+        int i = 0, j = len-1;
+        while(i <= j)
+        {
+            if(s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+        // Your code goes here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    // your code goes here
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int K;
+        cin >> K;
+        struct Node *head = NULL;
+        struct Node *temp = head;
+
+        for (int i = 0; i < K; i++) {
+            string str = "";
+            cin >> str;
+            if (head == NULL)
+                head = temp = new Node(str);
+            else {
+                temp->next = new Node(str);
+                temp = temp->next;
+            }
+        }
+        Solution ob;
+        bool ans = ob.compute(head);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
