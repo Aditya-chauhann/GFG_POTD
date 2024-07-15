b0171c0cd2182a5cf929551a37a8baf90687bd01
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2024-07-16 01:06:30 +0530

    GFG_POTD

---
+++ July-2024/16-07-24.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        for(int i=0;i<s.length();++i){
+            if(s[i]==ch)count--;
+            if(count==0){
+                return s.substr(i+1,s.length()-1);
+            }
+        }
+        return "";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
