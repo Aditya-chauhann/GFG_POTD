110b935a811566f7babd6690ea3087e284751e30
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2024-07-29 12:46:47 +0530

    GFG_POTD

---
+++ July-2024/29-07-24.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        // code here
+        int big = 0, x = -1;
+        for(int i = 0; i<arr.size(); i++)
+        {
+            int cnt = 0;
+            for(int j : arr[i])
+                cnt+=j;
+            if(cnt > big)
+            {
+                big = cnt;
+                x = i;
+            }
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
